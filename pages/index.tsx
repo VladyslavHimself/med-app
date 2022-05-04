73b74e77ac2605022c2c2920b26d24a768d75530
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-03 20:35:00 -0400

    package updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>VITech Med App</title>
-      </Head>
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>VITech Med App</title>
+            </Head>
+        </div>
+    );
 };
 
 export default Home;
