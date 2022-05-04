6f4d67713888ca49f1900786b7ac8e5cca17fcf0
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-04 18:45:43 -0400

    Added comments type

--- interfaces/IPatient.interface.ts
+++ interfaces/IPatient.interface.ts
@@ -1,14 +1,22 @@
-type gender = 'male' | 'female';
+type Gender = 'male' | 'female';
+type Country = 'USA' | 'Canada' | 'Ukraine';
 
-type country = 'USA' | 'Canada' | 'Ukraine';
+export interface IComment {
+    date: Date;
+    comment: string;
+}
 
 export interface IPatient {
     id: number;
     name: string;
     surname: string;
-    birthDate: Date;
-    gender: gender;
 
-    country: country;
+    birthDate: Date;
+    gender: Gender;
+
+    country: Country;
+    state?: string;
     address: string;
+
+    comments?: IComment[];
 }
