3971d6815133b00171fbe05df4ba00f66bdb55ae
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-04 17:14:08 -0400

    added contact and patients list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+import classes from '../scss/pages/index.module.scss';
+import PatientsList from '../src/components/PatientsList';
+import SearchField from '../src/components/SearchField';
 
 const Home: NextPage = () => {
+    const [patientsData, setPatientsData] = useState([]);
+    const [searchInput, setSearchInput] = useState<string>('');
+
+    const onAddNewPatientHandle = (): void => {
+        // TODO: Add new patient
+        setSearchInput('');
+    };
+
     return (
         <div>
             <Head>
                 <title>VITech Med App</title>
             </Head>
+
+            <div className={classes.contacts}>
+                <div className={classes.container}>
+                    <div className={classes['search-patient']}>
+                        <SearchField
+                            buttonValue="New Patient"
+                            inputState={searchInput}
+                            setInputState={setSearchInput}
+                            onButtonClickHandler={onAddNewPatientHandle}
+                        />
+                    </div>
+
+                    <hr />
+
+                    <PatientsList />
+                </div>
+            </div>
         </div>
     );
 };
