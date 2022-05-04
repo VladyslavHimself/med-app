8a16a0b3f5f0f941276e70c8ea780c9d3193164b
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-03 20:45:56 -0400

    initialized mui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <meta name="description" content="VITech Test Task" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html>
+            <Head>
+                <meta name="description" content="VITech Test Task" />
+
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
