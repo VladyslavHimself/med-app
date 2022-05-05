425c13938dd14f9e71e86efab9b3ae96fa56e1f5
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-05 18:29:43 -0400

    added helper functions

---
+++ src/services/firebase/firebase.service.ts
@@ -0,0 +1,32 @@
+import { firestoreDb as db } from '../../configs/firebase';
+import { IPatient } from '../../interfaces/IPatient.interface';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+
+export async function addNewPatient(patient: IPatient) {
+    const { name, surname, birthDate, gender, country, state, address, comments } = patient;
+
+    try {
+        const docRef = await addDoc(collection(db, 'patients'), {
+            name,
+            surname,
+            birthDate,
+            gender,
+            country,
+            state,
+            address,
+            comments,
+        });
+        console.log('Document written with ID: ', docRef.id);
+    } catch (e) {
+        console.error('Error adding document: ', e);
+    }
+}
+
+export async function getPatients(): Promise<IPatient[]> {
+    const dataSnapshot = await getDocs(collection(db, 'patients'));
+    const patients: IPatient[] = [];
+    dataSnapshot.forEach((doc) => {
+        patients.push(doc.data() as IPatient);
+    });
+    return patients;
+}
