983b451e0f9bdb59ce4446fc95134964ac2f85e1
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-06 11:34:37 -0400

    added date to timestamp convertation & typyzied functions

--- src/utils/date/date.service.ts
+++ src/utils/date/date.service.ts
@@ -1,10 +1,14 @@
 import { Timestamp } from 'firebase/firestore';
 
-export const getDateByDefaultFormat = (date: Timestamp): any => {
+export const getDateByDefaultFormat = (date: Timestamp): string => {
     const inputDate = date.toDate();
     const yyyy = inputDate.getFullYear();
     const mm: string = inputDate.toLocaleString('en-us', { month: 'long' }).slice(0, 3);
     let dd: number | string = inputDate.getDate();
 
     return `${dd} ${mm}, ${yyyy}`;
+};
+
+export const convertDateToTimestamp = (date: Date): Timestamp => {
+    return Timestamp.fromDate(date);
 };
