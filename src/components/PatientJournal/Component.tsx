3cbf1aa486752a72e760aaaed7bc9bd46738f26c
vladyslavhimself <vladyslavhimself@gmail.com>
2022-05-07 14:45:18 -0400

    added input field

--- src/components/PatientJournal/Component.tsx
+++ src/components/PatientJournal/Component.tsx
@@ -1,11 +1,27 @@
+import { Button, Fab, Input, TextField } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 import classes from './styles.module.scss';
 
-import { IPatient } from '../../interfaces/IPatient.interface';
+const inputStyles = {
+    input: { color: '#fff' },
+    label: { color: '#fff' },
+};
+
+// #TODO: need textField comopnent scalability
 
 function PatientJournal({ selectedPatient }: any) {
     return (
-        <div className={classes.journal}>
-            <h1>Patient Journal</h1>
+        <div className={classes['journal-container']}>
+            <div className={classes.journal}>
+                <p className={classes.journal__header}>Comments:</p>
+                <div className={classes.journal__comments}>comm fields</div>
+                <div className={classes['journal__input-field']}>
+                    <TextField sx={inputStyles} type="text" variant="filled" placeholder="Add new comment" fullWidth />
+                    <Fab sx={{ borderRadius: '0px' }} color="primary" aria-label="add">
+                        <SendIcon />
+                    </Fab>
+                </div>
+            </div>
         </div>
     );
 }
